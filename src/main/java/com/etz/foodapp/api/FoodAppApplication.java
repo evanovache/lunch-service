8cdbbba059be84f9b0e816c9d30ba2ea563791c8
Evans Keborni <evanovache78@gmail.com>
2026-02-17 05:25:51 +0000

    Add JAX-RS configuration

---
+++ src/main/java/com/etz/foodapp/api/FoodAppApplication.java
@@ -0,0 +1,7 @@
+package com.etz.foodapp.api;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class FoodAppApplication extends Application { }
